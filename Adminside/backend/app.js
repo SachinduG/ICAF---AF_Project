5a9c855e4a84f45446ca5847c62630010c39ddb5
Sachindu <sachindugimhana0@gmail.com>
2021-04-30 12:51:25 +0530

    server connected to monogodb

---
+++ Adminside/backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//set up server
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+//connect to monogoDB
+mongoose.connect(
+    process.env.MDB_CONNECT,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) =>{
+    if(err) return console.error(err);
+    console.log("Connected to MongoDB");
+});
