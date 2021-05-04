045fdb00c96d708a5f84f1b3f0cd6e9fb12887b8
Sachindu <sachindugimhana0@gmail.com>
2021-05-04 19:22:59 +0530

    login route created

--- Adminside/backend/routes/userRoute.js
+++ Adminside/backend/routes/userRoute.js
@@ -3,6 +3,7 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//register
 router.post("/", async (req, res) => {
     try {
         const {email, password, passwordVerify} = req.body;
@@ -61,4 +62,62 @@ router.post("/", async (req, res) => {
     }
 });
 
+//login
+
+router.post("/login", async (req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        //validate
+        if(!email || !password){
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields."});
+
+            const existingUser = await User.findOne({ email });
+            if(!existingUser){
+                return res
+                    .status(401)
+                    .json({ errorMessage: "Wrong email or password."});
+            }
+
+            const passwordCorrect = await  bcrypt.compare(
+                password,
+                existingUser.passwordHash
+            );
+            if(!passwordCorrect){
+                return res
+                    .status(401)
+                    .json({ errorMessage: "Wrong email or password."});
+            }
+
+            //sign the token
+            const token = jwt.sign(
+                {
+                user: existingUser._id,
+                },
+                process.env.JWT_SECRET
+            );
+
+            //send the token in a HTTP-only cookie
+            res.
+                cookie("token", token, {
+                httpOnly: true,
+            })
+                .send();
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+router.get("/logout", (req, res) => {
+    res.
+        cookie("token", "",{
+            httpOnly: true,
+            expire: new Date(0)
+    })
+        .send();
+});
 module.exports = router;
