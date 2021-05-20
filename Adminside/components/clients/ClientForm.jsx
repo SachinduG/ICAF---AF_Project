7b4d44eb80968522990d20ba9822b57390898600
Sachindu <sachindugimhana0@gmail.com>
2021-05-20 14:46:19 +0530

    clients

--- Adminside/components/clients/ClientForm.jsx
+++ Adminside/components/clients/ClientForm.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
 function ClientForm(){
-    return
-    <div>
+    const [clientName, setClientName] = useState("");
 
-    </div>;
+    async function saveClient(e){
+        e.preventDefault();
+
+        try{
+            const clientData = {
+                name: clientName,
+            };
+            await axios.post("https://localhost/5000/client/", clientData);
+        }catch (err){
+            console.error(err);
+        }
+    }
+}
+function ClientForm(){
+    return (
+        <div>
+            <form onSubmit={saveClient}>
+            <input
+                type="text"
+                placeholder="Client Name"
+                onChange={(e) => {
+                    setClientName(e.target.value);
+                }}
+                value={clientName}
+            />
+            <button type="submit">Save</button>
+            </form>
+        </div>
+    );
 };
 
 export default ClientForm;
--- Adminside/components/clients/ClientList.jsx
+++ Adminside/components/clients/ClientList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ClientForm from "./ClientForm";
 
+fun
 function ClientList(){
     return (
         <div>
-
+            <ul>
+                <ClientForm/>
+                <ClientList/>
+            </ul>
         </div>
     );
 };
