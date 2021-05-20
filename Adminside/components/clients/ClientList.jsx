7abeb12e9bf849d71cc4c52059aa4a00341c77b3
Sachindu <sachindugimhana0@gmail.com>
2021-05-20 14:50:58 +0530

    clients list

--- Adminside/components/clients/ClientList.jsx
+++ Adminside/components/clients/ClientList.jsx
@@ -1,12 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ClientForm from "./ClientForm";
 
 function Client(){
     const [clients, setClients] = useState([]);
 
     async function getClients(){
-        const clientsRes = await axios.get("https://localhost:5000/client/", cl);
+        const clientsRes = await axios.get("https://localhost:5000/client/");
+        setClients(clientsRes.data);
     }
+
+    useEffect(() =>{
+        getClients()
+    }, []);
 }
 function ClientList(){
     return (
