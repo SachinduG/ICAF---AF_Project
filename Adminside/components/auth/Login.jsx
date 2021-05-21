38a93b32fff3db4e494c46620259031f05a92557
Sachindu <sachindugimhana0@gmail.com>
2021-05-21 12:47:38 +0530

    navbar designed

--- Adminside/components/auth/Login.jsx
+++ Adminside/components/auth/Login.jsx
@@ -3,6 +3,32 @@ import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import Swal from "sweetalert2";
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    withoutLabel: {
+        marginTop: theme.spacing(3),
+    },
+    textField: {
+        width: '25ch',
+    },
+}));
 
 function Login() {
 
@@ -37,15 +63,57 @@ function Login() {
         }
     }
 
+    const [values, setValues] = React.useState({
+        password: '',
+        showPassword: false,
+    });
+
+    const handleChange = (props) => (event) => {
+        setValues({ ...values, [props]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+
+        const handleMouseDownPassword = (event) => {
+            event.preventDefault();
+        };
+
+
     return (
         <div>
             <h1>Log in to your account</h1>
             <form onSubmit={login}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
+                <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                        <AccountCircle />
+                    </Grid>
+                    <Grid item>
+                        <TextField id="input-with-icon-grid" label="Email Address"
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+                    </Grid>
+                </Grid>
+                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                    id="standard-adornment-password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                            >
+                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
                 />
                 <input
                     type="password"
--- Adminside/components/auth/Register.jsx
+++ Adminside/components/auth/Register.jsx
@@ -3,8 +3,20 @@ import axios from "axios";
 import AuthContext from "../../context/AuthContext";
 import { useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
+import { makeStyles } from '@material-ui/core/styles';
+import {TextField} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 function Register() {
+    const classes = useStyles();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -41,21 +53,21 @@ function Register() {
     return(
         <div>
             <h1>Register a new account</h1>
-            <form onSubmit={register}>
-                <input type="email"
-                       placeholder="Email Address"
-                       onChange={(e) => setEmail(e.target.value)}
-                       value={email}
+            <form onSubmit={register} className={classes.root}>
+                <TextField id="outlined-basic" label="Email Address" variant="outlined" required
+                           type="email"
+                           onChange={(e) => setEmail(e.target.value)}
+                           value={email}
                 />
-                <input type="password"
-                       placeholder="Password"
-                       onChange={(e) => setPassword(e.target.value)}
-                       value={password}
+                <TextField id="outlined-basic" label="Password" variant="outlined" required
+                           type="password"
+                           onChange={(e) => setPassword(e.target.value)}
+                           value={password}
                 />
-                <input type="password"
-                       placeholder="Confirm-Password"
-                       onChange={(e) => setPasswordVerify(e.target.value)}
-                       value={passwordVerify}
+                <TextField id="outlined-basic" label="Confirm Password" variant="outlined" required
+                           type="password"
+                           onChange={(e) => setPasswordVerify(e.target.value)}
+                           value={passwordVerify}
                 />
                 <button type="submit" className="btn btn-outline-secondary">Register</button>
         </form>
