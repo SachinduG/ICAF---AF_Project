740c18022951b6a03b418c8dd423dc0a890b7aab
Sachindu <sachindugimhana0@gmail.com>
2021-05-21 14:18:17 +0530

    text field designed

--- Adminside/components/layout/Navbar.jsx
+++ Adminside/components/layout/Navbar.jsx
@@ -2,27 +2,53 @@ import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import LogOutBtn from "../auth/LogOutBtn";
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        color: "black"
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function Navbar(){
+    const classes = useStyles();
+
     const { loggedIn } = useContext(AuthContext);
+
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="nav-link" href="/" ><HomeRoundedIcon/><span className="sr-only">(current)</span> </a>
                 {loggedIn === false && (
                     <>
                         <a className="nav-link" href="/register">Register</a>
                         <a className="nav-link" href="/login">Log In</a>
                     </>
                 )}
-
-                {loggedIn === true && (
-                    <>
-                        <a className="nav-link" href="/client">Clients</a>
-                        <LogOutBtn/>
-                    </>
-                )}
             </nav>
+            <div className={classes.root}>
+                {loggedIn === true && (
+                        <AppBar position="static">
+                            <Toolbar>
+                                <a className="nav-link" href="/" ><HomeRoundedIcon/><span className="sr-only">(current)</span> </a>
+                                <Typography variant="h6" className={classes.title} >
+                                    <a className="nav-link" href="/client">Clients</a>
+                                </Typography>
+                                <LogOutBtn/>
+                            </Toolbar>
+                        </AppBar>
+
+                )}
+            </div>
         </div>
     );
 }
