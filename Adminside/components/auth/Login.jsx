be6162626fcc35df194450554b8d13ab8ec8bd48
Sachindu <sachindugimhana0@gmail.com>
2021-05-21 13:04:17 +0530

    text field designed

--- Adminside/components/auth/Login.jsx
+++ Adminside/components/auth/Login.jsx
@@ -4,30 +4,15 @@ import { useHistory } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import Swal from "sweetalert2";
 import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 const useStyles = makeStyles((theme) => ({
     margin: {
         margin: theme.spacing(1),
-    },
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    withoutLabel: {
-        marginTop: theme.spacing(3),
-    },
-    textField: {
-        width: '25ch',
-    },
+    }
 }));
 
 function Login() {
@@ -63,24 +48,6 @@ function Login() {
         }
     }
 
-    const [values, setValues] = React.useState({
-        password: '',
-        showPassword: false,
-    });
-
-    const handleChange = (props) => (event) => {
-        setValues({ ...values, [props]: event.target.value });
-    };
-
-    const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
-    };
-
-        const handleMouseDownPassword = (event) => {
-            event.preventDefault();
-        };
-
-
     return (
         <div>
             <h1>Log in to your account</h1>
@@ -97,30 +64,19 @@ function Login() {
                         />
                     </Grid>
                 </Grid>
-                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                <Input
-                    id="standard-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    onChange={handleChange('password')}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                            >
-                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                />
+                <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                        <LockOpenIcon />
+                    </Grid>
+                    <Grid item>
+                        <TextField id="input-with-icon-grid" label="Password"
+                                   type="password"
+                                   onChange={(e) => setPassword(e.target.value)}
+                                   value={password}
+                        />
+                    </Grid>
+                </Grid>
+
                 <button type="submit" className="btn btn-outline-success" >Log in</button>
             </form>
         </div>
