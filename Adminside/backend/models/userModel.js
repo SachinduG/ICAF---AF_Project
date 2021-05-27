76f0ede73c60a1b8d50a6b463dcbdca41111741f
Sachindu <sachindugimhana0@gmail.com>
2021-05-27 09:39:52 +0530

    install validator

--- Adminside/backend/models/userModel.js
+++ Adminside/backend/models/userModel.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-    fname: {type: String, required: true},
-    lname: {type: String, required: true},
-    email: {type: String, required: true},
-    passwordHash: {type: String, required: true},
+    fname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Please enter valid email address");
+            }
+        },},
+    passwordHash: {
+        type: String,
+        required: true,
+        trim: true,},
 });
 
 const User = mongoose.model("admin", userSchema);
