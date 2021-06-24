ef259bc4836c78c6fdeca97a1f7b6aee97626376
Sachindu <sachindugimhana0@gmail.com>
2021-06-24 14:26:45 +0530

    fix

--- Adminside/components/home/home.jsx
+++ Adminside/components/home/home.jsx
@@ -7,13 +7,9 @@ function Home() {
             <div class="container">
                 <div class="row align-items-center my-10">
                     <div class="col-lg-7">
-                        <img
-                            class="img-fluid rounded mb-4 mb-lg-0"
-                            src="http://placehold.it/900x400"
-                            alt=""
-                        />
+                        
                     </div>
-                    <div class="col-lg-5">
+                    <div class="col-lg-3">
                         <h1 class="font-weight-light">Home</h1>
                         <p>
                             Lorem Ipsum is simply dummy text of the printing and typesetting
