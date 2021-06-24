4eb3ef50f50edca99594617aa8c294736ce8437d
Sachindu <sachindugimhana0@gmail.com>
2021-06-24 13:47:29 +0530

    fixes

--- Adminside/components/auth/Register.jsx
+++ Adminside/components/auth/Register.jsx
@@ -7,8 +7,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Container, CssBaseline, TextField } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-
 
 const useStyles = makeStyles((theme) => ({
     root: {
