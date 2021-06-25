a2894c07e78786c901f1f305eee8f5f7d2d80fe7
Sachindu <sachindugimhana0@gmail.com>
2021-06-25 13:44:17 +0530

    fix

--- Adminside/components/home/home.jsx
+++ Adminside/components/home/home.jsx
@@ -23,7 +23,12 @@ function Home() {
                     </div>
                     <div className="col-lg-4">
                         <h1 className="font-weight-light">Contents</h1>
-                        <p>Research Papers</p>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting
+                            industry. Lorem Ipsum has been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of
+                            type and scrambled it to make a type specimen book.
+</p>
                     </div>
                 </div>
             </div>
