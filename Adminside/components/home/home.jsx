ea49d3b49eeac0b76965bdbec009cf7229a55d76
Sachindu <sachindugimhana0@gmail.com>
2021-06-25 13:43:44 +0530

    fix

--- Adminside/components/home/home.jsx
+++ Adminside/components/home/home.jsx
@@ -1,40 +1,6 @@
-import Axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
-import Attendee from "../clients/Attendee";
-import AuthContext from "../../context/AuthContext";
+import React from "react";
 
 function Home() {
-    const [attendees, setAttendees] = useState([]);
-
-  const { loggedIn } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (!loggedIn) setAttendees([]);
-    else getAttendees();
-  }, [loggedIn]);
-
-  async function getAttendees() {
-    const attendeesRes = await Axios.get(`http://localhost:5000/attendee/`);
-    setAttendees(attendeesRes.data);
-  }
-
-  function renderAttendees() {
-    let sortedAttendees = [...attendees];
-    sortedAttendees = sortedAttendees.sort((a, b) => {
-      return new Date(b.createdAt) - new Date(a.createdAt);
-    });
-
-    return sortedAttendees.map((attendee, i) => {
-      return (
-        <Attendee
-          key={i}
-          attendee={attendee}
-          getAttendees={getAttendees}
-        />
-      );
-    });
-  }
-
     return (
         <div className="home">
             <div className="container">
@@ -54,13 +20,10 @@ function Home() {
                         <p>Researchers</p>
                         <p>Presenters</p>
                         <p>Attendees</p>
-                            {attendees.length > 0 ? renderAttendees() : loggedIn &&
-                            (<p className="alert alert-warning" role="alert" style={{ marginTop: 40 }}>No attendees have been added yet.</p>)}
                     </div>
                     <div className="col-lg-4">
                         <h1 className="font-weight-light">Contents</h1>
                         <p>Research Papers</p>
-                        <p>Workshops</p>
                     </div>
                 </div>
             </div>
