d6ae3dfa939b21310e38e9f24fa5249285431c0f
Sachindu <sachindugimhana0@gmail.com>
2021-06-30 13:47:45 +0530

    commited

--- Adminside/components/home/home.jsx
+++ Adminside/components/home/home.jsx
@@ -1,31 +1,42 @@
 import React from "react";
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button,
+    Row,
+    Col
+} from 'reactstrap';
 
 function Home() {
     return (
         <div className="home">
             <div className="container">
                 <h1 className="font-weight"><center>Welcome to the Admin Dashboard</center></h1>
-                <div className="row align-items-center my-5">
-                    <div className="col-lg-4">
-                        <h1 className="font-weight-light">Home</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
-                        </p>
-                    </div>
-                    <div className="col-lg-4">
-                        <h1 className="font-weight">Clients</h1>
+                <div>
+                    <Row>
+                        <Col xs="12" md="4">
+                            <Feeds />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs="12" md="4">
+                            <h1 className="font-weight">Clients</h1>
                             <h6 className="font-weight-light">Researchers</h6>
                             <h6 className="font-weight-light">Presenters</h6>
                             <h6 className="font-weight-light">Attendees</h6>
-                    </div>
-                    <div className="col-lg-4">
-                        <h1 className="font-weight">Events</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs="12" md="4">
+                            <h1 className="font-weight">Events</h1>
                             <h6 className="font-weight-light">Researcher Papers</h6>
                             <h6 className="font-weight-light">Workshops</h6>
-                    </div>
+                        </Col>
+                    </Row>
                 </div>
             </div>
         </div>
